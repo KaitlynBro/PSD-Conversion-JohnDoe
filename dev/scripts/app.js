106cd47af3d631eb461c32e15cd8cbfb4808b522
Kaitlyn Brouwers <kaitlynnicoleb@hotmail.com>
2017-07-08 16:29:57 -0400

    add

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,1 +1,19 @@
-console.log('test')
+const john = {}
+
+john.init = function() {
+	john.getJohn();
+}
+
+console.log('hiiiii');
+
+john.getJohn = function() {
+	$(".submitButton").on('click', function() {
+	    $('html,body').animate({
+	        scrollTop: $(".followMe").offset().top},
+	        'slow');
+	});
+};
+
+$(document).ready(function() {
+	john.init();
+});
